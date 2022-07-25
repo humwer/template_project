10d8bfba10c1cb5f4b8d8b71c686e321abc5485d
humwer <lgmzrk@inbox.ru>
2022-07-25 19:00:11 +0700

    Added error handling WebDriverException and some fixes

--- webwalker.py
+++ webwalker.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.support import expected_conditions as EC
 from fake_useragent import UserAgent
 from random import randrange
@@ -12,9 +12,9 @@ def real_user(method):
     def sleeping(*args, **kwargs):
         rand_int = randrange(3, 5)
         if rand_int == 1:
-            print(f'[?] Please, wait for {rand_int} second')
+            print(f'[*] Please, wait for {rand_int} second')
         else:
-            print(f'[?] Please, wait for {rand_int} seconds')
+            print(f'[*] Please, wait for {rand_int} seconds')
         time.sleep(rand_int)
         method(*args, **kwargs)
     return sleeping
@@ -27,6 +27,7 @@ class WebWalker:
         self.options = Options()
         self.options.add_argument(f'user-agent={UserAgent().random}')
         self.browser = webdriver.Chrome(options=self.options)
+        self.last_error = None
         self.wait_time = 10
         print("[+] Opened browser Google Chrome")
 
@@ -39,32 +40,58 @@ class WebWalker:
         try:
             self.browser.get(url)
             print(f'[+] Went to: {url}')
+            self.last_error = None
         except ValueError:
             print('[-] I cant go here ><')
+            self.last_error = ValueError
 
     @real_user
     def click_this_button(self, selectors: tuple):
         try:
-            button = WebDriverWait(self.browser, self.wait_time).until(EC.presence_of_element_located(selectors))
-            print(f"[+] Clicked button: '{button.text}'")
-            button.click()
+            if self.last_error is None:
+                button = WebDriverWait(self.browser, self.wait_time).until(EC.presence_of_element_located(selectors))
+                print(f"[+] Clicked button: '{button.text}'")
+                button.click()
+            else:
+                print("[x] Hey, I dont will do that while you not use func 'go_to_url',\n"
+                      f"Because we have error: {self.last_error}")
         except TimeoutException:
             print(f"[-] My bad, I don't find '{selectors}' -> button ._.")
+            self.last_error = TimeoutException
+        except WebDriverException:
+            print("[-] Wow, error. Maybe we have low timeout?")
+            self.last_error = WebDriverException
 
     @real_user
     def text_of_the_element(self, selectors: tuple):
         try:
-            element = WebDriverWait(self.browser, self.wait_time).until(EC.presence_of_element_located(selectors))
-            print(f"[+] Text of the element: '{element.text}'")
-            return element.text
+            if self.last_error is None:
+                element = WebDriverWait(self.browser, self.wait_time).until(EC.presence_of_element_located(selectors))
+                print(f"[+] Text of the element: '{element.text}'")
+                return element.text
+            else:
+                print("[x] Hey, I dont will do that while you not use func 'go_to_url',\n"
+                      f"Because we have error: {self.last_error}")
         except TimeoutException:
             print(f"[-] My bad, I don't find '{selectors}' -> element ._.")
+            self.last_error = TimeoutException
+        except WebDriverException:
+            print("[-] Wow, error. Maybe we have low timeout?")
+            self.last_error = WebDriverException
 
     @real_user
     def fill_this_element(self, selectors: tuple, fill_text: str = None):
         try:
-            element = WebDriverWait(self.browser, self.wait_time).until(EC.presence_of_element_located(selectors))
-            element.send_keys(fill_text)
-            print(f"[+] Text '{fill_text}' in the element")
+            if self.last_error is None:
+                element = WebDriverWait(self.browser, self.wait_time).until(EC.presence_of_element_located(selectors))
+                element.send_keys(fill_text)
+                print(f"[+] Text '{fill_text}' in the element")
+            else:
+                print("[x] Hey, I dont will do that while you not use func 'go_to_url',\n"
+                      f"Because we have error: {self.last_error}")
         except TimeoutException:
             print(f"[-] My bad, I cant set text '{fill_text}'")
+            self.last_error = TimeoutException
+        except WebDriverException:
+            print("[-] Wow, error. Maybe we have low timeout?")
+            self.last_error = WebDriverException
