d6865b2b185ec0591e6f68f1526fb372dcb5eb1c
humwer <lgmzrk@inbox.ru>
2022-07-25 18:10:20 +0700

    Added file webwalker with structure

---
+++ webwalker.py
@@ -0,0 +1,21 @@
+
+
+class WebWalker:
+
+    def __init__(self, url: str):
+        pass
+
+    def __del__(self):
+        pass
+
+    def go_to_url(self, url: str = None):
+        pass
+
+    def click_this_button(self, selectors: tuple):
+        pass
+
+    def text_of_the_element(self, selectors: tuple):
+        pass
+
+    def fill_this_element(self, selectors: tuple, fill_text: str = None):
+        pass
