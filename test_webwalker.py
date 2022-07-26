b6c6994aa48b630975f0f119a1143fb9a319b6cd
humwer <lgmzrk@inbox.ru>
2022-07-26 16:42:33 +0700

    Added test of check url after surf to site

---
+++ test_webwalker.py
@@ -0,0 +1,7 @@
+from locators import TestSite
+
+
+def test_guest_surfed_on_site(walker):
+    walker.go_to_url()
+    assert walker.get_current_url() == TestSite.TEST_URL, "[-] Its not main page"
+    print("[+] Success. We surfed to main page.")
