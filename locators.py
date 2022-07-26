b47e9e282f9f98aad9152c01ecbd580d45eca111
humwer <lgmzrk@inbox.ru>
2022-07-26 16:40:24 +0700

    Changed locators for test website

--- locators.py
+++ locators.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
 
 
-class GitHubPage:
-    GITHUB_TEST_URL = "https://github.com/"
-    GITHUB_TEST_INPUT = (By.CSS_SELECTOR, "input[data-test-selector=nav-search-input]")
-    GITHUB_TEST_CLICK = (By.CSS_SELECTOR, "div[aria-label=humwer]")
+class TestSite:
+    TEST_URL = "http://automationpractice.com/index.php"
+    TEST_BUTTON_GO_TO_CATALOG = (By.CSS_SELECTOR, "a[title=Women]")
+    TEST_IS_WOMEN_PAGE = (By.CSS_SELECTOR, ".navigation_page")
+    TEST_CATEGORY_TOPS = (By.CSS_SELECTOR, "#layered_category_4")
+    TEST_CATEGORY_DRESSES = (By.CSS_SELECTOR, "#layered_category_8")
+    TEST_HAS_CATEGORY = (By.CSS_SELECTOR, ".cat-name")
 
 
