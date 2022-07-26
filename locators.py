c56593557b60ec3ef6f23950522d63e36d1bc88d
humwer <lgmzrk@inbox.ru>
2022-07-26 16:44:58 +0700

    Fixed names of locators

--- locators.py
+++ locators.py
@@ -2,11 +2,11 @@ from selenium.webdriver.common.by import By
 
 
 class TestSite:
-    TEST_URL = "http://automationpractice.com/index.php"
-    TEST_BUTTON_GO_TO_CATALOG = (By.CSS_SELECTOR, "a[title=Women]")
-    TEST_IS_WOMEN_PAGE = (By.CSS_SELECTOR, ".navigation_page")
-    TEST_CATEGORY_TOPS = (By.CSS_SELECTOR, "#layered_category_4")
-    TEST_CATEGORY_DRESSES = (By.CSS_SELECTOR, "#layered_category_8")
-    TEST_HAS_CATEGORY = (By.CSS_SELECTOR, ".cat-name")
+    URL = "http://automationpractice.com/index.php"
+    BUTTON_GO_TO_CATALOG = (By.CSS_SELECTOR, "a[title=Women]")
+    IS_CATALOG = (By.CSS_SELECTOR, ".navigation_page")
+    CATEGORY_TOPS = (By.CSS_SELECTOR, "#layered_category_4")
+    CATEGORY_DRESSES = (By.CSS_SELECTOR, "#layered_category_8")
+    SELECTED_CATEGORIES = (By.CSS_SELECTOR, ".cat-name")
 
 
