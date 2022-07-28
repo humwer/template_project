1390eb4546b754f3310519d6f200f2aa934eb74d
humwer <lgmzrk@inbox.ru>
2022-07-28 12:49:15 +0700

    Added locator of cookies

--- examples/hltv/locators.py
+++ examples/hltv/locators.py
@@ -3,4 +3,5 @@ from selenium.webdriver.common.by import By
 
 class HLTVLocators:
     URL = 'https://www.hltv.org/ranking/teams/'
+    COOKIES_BUTTON = (By.CSS_SELECTOR, "#CybotCookiebotDialogBodyButtonDecline")
     RATING = (By.CSS_SELECTOR, ".regional-ranking-header")
