93c8bd894204a0863dd531dc00534ee2e786bb0b
humwer <lgmzrk@inbox.ru>
2022-07-28 12:58:37 +0700

    Added locators for teams names and points

--- examples/hltv/locators.py
+++ examples/hltv/locators.py
@@ -5,3 +5,5 @@ class HLTVLocators:
     URL = 'https://www.hltv.org/ranking/teams/'
     COOKIES_BUTTON = (By.CSS_SELECTOR, "#CybotCookiebotDialogBodyButtonDecline")
     RATING = (By.CSS_SELECTOR, ".regional-ranking-header")
+    TEAM_NAME = (By.CSS_SELECTOR, ".teamLine > .name")
+    TEAM_POINTS = (By.CSS_SELECTOR, ".teamLine > .points")
