2fc09cafb8ecc91667e078eb4783080ddce3f57c
humwer <lgmzrk@inbox.ru>
2022-08-01 17:00:16 +0700

    Added new locator of team's position

--- examples/hltv/locators.py
+++ examples/hltv/locators.py
@@ -5,6 +5,7 @@ class HLTVLocators:
     URL = 'https://www.hltv.org/ranking/teams/'
     COOKIES_BUTTON = (By.CSS_SELECTOR, "#CybotCookiebotDialogBodyButtonDecline")
     RATING = (By.CSS_SELECTOR, ".regional-ranking-header")
+    TEAM_POSITION = (By.CSS_SELECTOR, ".ranking-header .position")
     TEAM_NAME = (By.CSS_SELECTOR, ".teamLine > .name")
     TEAM_POINTS = (By.CSS_SELECTOR, ".teamLine > .points")
     TEAM_PLAYERS = (By.CSS_SELECTOR, ".playersLine")
